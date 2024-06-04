13c4e737393d5b11984a1df8969b48413c69e8b7
BhargavDankhara <154862656+BhargavDankhara@users.noreply.github.com>
2024-06-04 23:13:02 +0530

    user auth setup using Auth0

--- src/components/MainNave.tsx
+++ src/components/MainNave.tsx
@@ -1,9 +1,22 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
+import UsernameMenu from "./UsernameMenu";
 
 export default function MainNave() {
+
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
+
   return (
-    <Button variant="ghost" className="font-bold hover:text-orange-500 hover:bg-white text-xl">
-        Log In 
-    </Button>
+    <span className="flex space-x-2 items-center">
+      {isAuthenticated
+        ? <UsernameMenu />
+        : <Button variant="ghost"
+          className="font-bold hover:text-orange-500 hover:bg-white text-xl"
+          onClick={async () => await loginWithRedirect()}
+        >
+          Log In
+        </Button>}
+    </span>
+
   )
 }
