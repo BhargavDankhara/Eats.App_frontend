f522bb1e253e2878fe9d6d2ad5d2fa120e715061
BhargavDankhara <154862656+BhargavDankhara@users.noreply.github.com>
2024-06-06 17:55:08 +0530

    authenticated user links, login, logout

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -1,9 +1,14 @@
-import { Menu } from "lucide-react";
+import { CircleUserRound, Menu } from "lucide-react";
 import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from "./ui/sheet";
 import { Separator } from "./ui/separator";
 import { Button } from "./ui/button";
+import { useAuth0 } from "@auth0/auth0-react";
+import MobileNavLinks from "./MobileNavLinks";
 
 export default function MobileNav() {
+
+    const { isAuthenticated, loginWithRedirect, user } = useAuth0();
+
     return (
         <Sheet>
             <SheetTrigger>
@@ -11,11 +16,25 @@ export default function MobileNav() {
             </SheetTrigger>
             <SheetContent className="space-y-3">
                 <SheetTitle>
-                    <span>Welcome To Eats.com!!</span>
+                    {isAuthenticated
+                        ? <span className="flex items-center font-bold gap-2">
+                            <CircleUserRound className="text-orange-500" />
+                            {user?.email}
+                        </span>
+                        : <span>Welcome To Eats.com!!</span>
+                    }
+
                 </SheetTitle>
                 <Separator />
-                <SheetDescription className="flex">
-                    <Button className="flex-1 font-bold bg-orange-500">Log In</Button>
+                <SheetDescription className="flex flex-col gap-4">
+                    {isAuthenticated
+                        ? <MobileNavLinks />
+                        : <Button
+                            onClick={() => loginWithRedirect()}
+                            className="flex-1 font-bold bg-orange-500">
+                            Log In
+                        </Button>
+                    }
                 </SheetDescription>
             </SheetContent>
         </Sheet>
