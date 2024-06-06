b497b5f4a3b1f306f99f9b0ed42cf6edec464d4d
BhargavDankhara <154862656+BhargavDankhara@users.noreply.github.com>
2024-06-06 23:16:50 +0530

    Validated Access Token!!

--- src/auth/Auth0ProviderWithNavigate.tsx
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -12,8 +12,9 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
     const domain = import.meta.env.VITE_AUTH0_DOMAIN;
     const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
     const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URI;
+    const audience = import.meta.env.VITE_AUTH0_AUDIENCE
 
-    if (!domain || !clientId || !redirectUri) {
+    if (!domain || !clientId || !redirectUri || !audience) {
         throw new Error("unable to initialise auth");
     }
 
@@ -30,6 +31,7 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
             clientId={clientId}
             authorizationParams={{
                 redirect_uri: redirectUri,
+                audience,
             }}
             onRedirectCallback={onRedirectCallback}
         >
