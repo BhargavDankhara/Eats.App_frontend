e50dc1ce578ae2d109a4f0cca61f956e5dedcd4c
BhargavDankhara <154862656+BhargavDankhara@users.noreply.github.com>
2024-07-09 18:39:18 +0530

    toster msg added

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './global.css'
-import { BrowserRouter as Router } from 'react-router-dom'
-import AppRoutes from './AppRoutes'
-import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate'
-import { QueryClient, QueryClientProvider } from 'react-query'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./global.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./AppRoutes";
+import Auth0ProviderWithNavigate from "./auth/Auth0ProviderWithNavigate";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { Toaster } from "./components/ui/sonner";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -15,15 +16,16 @@ const queryClient = new QueryClient({
   },
 });
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Router>
       <QueryClientProvider client={queryClient}>
-        <Auth0ProviderWithNavigate >
+        <Auth0ProviderWithNavigate>
           <AppRoutes />
+          <Toaster visibleToasts={1} position="top-right" richColors />
+          //richColors updates color base on msg
         </Auth0ProviderWithNavigate>
       </QueryClientProvider>
-
     </Router>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
