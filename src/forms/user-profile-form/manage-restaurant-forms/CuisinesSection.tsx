107040e56ca1d09001ea98749b22c57258db5301
BhargavDankhara <154862656+BhargavDankhara@users.noreply.github.com>
2024-07-31 21:58:45 +0530

    restaurent form created!!

---
+++ src/forms/user-profile-form/manage-restaurant-forms/CuisinesSection.tsx
@@ -0,0 +1,38 @@
+import {
+  FormDescription,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { cuisineList } from "@/config/restaurent-option-config";
+import { useFormContext } from "react-hook-form";
+import CuisineCheckbox from "./CuisineCheckbox";
+
+export const CuisinesSection = () => {
+  const { control } = useFormContext();
+
+  return (
+    <div className="space-y-2">
+      <div>
+        <h2 className="text-2xl font-bols">Cuisines</h2>
+        <FormDescription>
+          Select the cuisines you restaurent serves
+        </FormDescription>
+      </div>
+      <FormField
+        control={control}
+        name="cuisines"
+        render={({ field }) => (
+          <FormItem>
+            <div className="grid md:grid-cols-5 gap-1">
+              {cuisineList.map((cuisineItem) => (
+                <CuisineCheckbox cuisine={cuisineItem} field={field} />
+              ))}
+            </div>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    </div>
+  );
+};
---
+++ src/forms/user-profile-form/manage-restaurant-forms/ImageSection.tsx
@@ -0,0 +1,47 @@
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { useFormContext } from "react-hook-form";
+
+export const ImageSection = () => {
+  const { control } = useFormContext();
+
+  return (
+    <div className="space-y-2">
+      <div>
+        <h2 className="text-2xl font-bold">Image</h2>
+        <FormDescription>
+          Add an cover image for your restaurant.
+        </FormDescription>
+      </div>
+      <div className="flex flex-col gap-8 w-[50%]">
+        <FormField
+          control={control}
+          name="image file"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input
+                  className="bg-white"
+                  type="file"
+                  accept=".jpg, .jepg, .png"
+                  onChange={(event) =>
+                    field.onChange(
+                      event.target.files ? event.target.files[0] : null
+                    )
+                  }
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+      </div>
+    </div>
+  );
+};
